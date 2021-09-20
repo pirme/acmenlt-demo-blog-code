3ccfc066d7cc917fffb07064a9d341831230abf4
chen.ma <machencoding@163.com>
2021-09-20 13:04:07 +0800

    Initialize commit.

---
+++ blog-code-oom/src/main/java/cn/acmenlt/demo/oom/OomApplication.java
@@ -0,0 +1,13 @@
+package cn.acmenlt.demo.oom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OomApplication.class, args);
+    }
+
+}
---
+++ blog-code-oom/src/test/java/cn/acmenlt/demo/oom/BlogCodeOomApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.acmenlt.demo.oom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCodeOomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
